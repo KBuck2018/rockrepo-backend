c138045f2e58023030332cdb1e56ff2eea71e9d9
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-04-19 13:55:12 -0400

    deploying backend

--- db/users.js
+++ db/users.js
@@ -1,8 +1,24 @@
 const mongoose = require("./connection.js");
 
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  comments: [CommentSchema]
 });
 
 mongoose.model("User", userSchema);
